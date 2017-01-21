9bec53c75d5d2c632631daae87a3dc14b2760939
Kegan Myers <kegan@keganmyers.com>
2017-01-21 10:54:08 -0600

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+// this should only be used for eslint config files
+// and ideally is never used outside of this project
+
+module.exports = {
+  extends: [
+    'airbnb-base/legacy',
+  ],
+  rules: {
+    'comma-dangle': ['error', 'always'],
+  }
+}
